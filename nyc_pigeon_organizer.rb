bbcb3f0e4e3d1c93315dd70c761e91cd6614d8f1
Yunhao Gao <as510891328@gmail.com>
2020-08-14 19:51:59 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,7 +6,7 @@ def nyc_pigeon_organizer(data)
   data.each{ |key, value| 
     value.each{ |i, value|
       value.each{ |value| 
-        if hash[value][key]
+        if hash[value]
           hash[value][key] << i.to_s
           binding.pry
         else
